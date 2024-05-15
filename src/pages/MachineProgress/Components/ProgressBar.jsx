00e70effcc564e90c1e60e8aaeb443644f568427
bhavyaverma08 <verma100bhavya@gmail.com>
2024-05-15 12:16:53 +0530

    ADD styling and FIX logic

--- src/pages/MachineProgress/Components/ProgressBar.jsx
+++ src/pages/MachineProgress/Components/ProgressBar.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const ProgressBar = (props) => {
   const { totalWidth = "500px", completedWidth = "30px" } = props;
   return (
-    <div style={{ width: totalWidth, background: "grey", height: "20px" }}>
-      <div
-        style={{ width: completedWidth, background: "green", height: "20px" }}
+    <div className="progress-bar" style={{ width: totalWidth, background: "#ececec"}}>
+      <div className="progress-bar"
+        style={{ width: completedWidth, background: "#00ab5a"}}
       ></div>
     </div>
   );
--- src/pages/MachineProgress/MachineProgress.jsx
+++ src/pages/MachineProgress/MachineProgress.jsx
@@ -1,33 +1,46 @@
 import React, { useEffect, useState } from "react";
 import ProgressBar from "./Components/ProgressBar";
+import "../MachineProgress/styles.css";
 
 const MachineProgress = (props) => {
   const { timeInterval = 500, percentageVariable = 0.05 } = props;
   let progressPercentage = 0.0;
   const [completedWidth, setCompletedWidth] = useState("0px");
-
-  useEffect(() => {
-    return () => {};
-  }, [progressPercentage]);
+  const [intervalID, setIntervalID] = useState(null);
 
   // Function definitions
   function handleStart() {
-    setInterval(() => {
+    const myInterval = setInterval(() => {
       progressPercentage = progressPercentage + percentageVariable;
-      setCompletedWidth((progressPercentage * 300).toString() + "px");
+      setCompletedWidth((progressPercentage * 500).toString() + "px");
+      console.log("progressPercentage",progressPercentage)
+      console.log("completedWidth",completedWidth)
+      if (Math.floor(progressPercentage) === 1) {
+        clearInterval(myInterval);
+      }
     }, timeInterval);
+    setIntervalID(myInterval);
+  }
+
+  function handlePause(){
+    clearInterval(intervalID)
+  }
+
+  function handleReset(){
+    setCompletedWidth("0px")
+    clearInterval(intervalID)
   }
 
   return (
-    <>
-      <h1>Machine-Progress</h1>
+    <div className="progress-container">
+      <h2>Machine-Progress</h2>
       <ProgressBar completedWidth={completedWidth} />
       <div className="button-container">
-        <button onClick={handleStart}>Start</button>
-        {/* <button onClick={handlePause}>Pause</button> */}
-        {/* <button onClick={handleStop}>Stop</button> */}
+        <button className="button-styles" onClick={handleStart}>Start</button>
+        <button className="button-styles"  onClick={handlePause}>Pause</button>
+        <button className="button-styles"  onClick={handleReset}>Reset</button>
       </div>
-    </>
+    </div>
   );
 };
 
