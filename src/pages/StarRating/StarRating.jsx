1646b1e4ed7e676b0601bf5e984d4b9df8590365
bhavyaverma08 <verma100bhavya@gmail.com>
2024-05-17 12:06:53 +0530

    ADD star rating functionality

--- src/pages/StarRating/StarRating.jsx
+++ src/pages/StarRating/StarRating.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./styles.css";
 
 const StarRating = () => {
-  return (
-    <div>StarRating</div>
-  )
-}
+  const [Hover, setHover] = useState(null);
+  const [clickedStar, setClickedStar] = useState(null);
+  function handleHover(str) {
+    setHover(str);
+  }
+  function handleClickStar(str) {
+    setClickedStar(str);
+  }
 
-export default StarRating
+  return (
+    <div className="star-rating-container">
+      <h2>Star Rating</h2>
+      <div className="star-container">
+        {[1, 2, 3, 4, 5].map((star) => {
+          return (
+            <span
+              key={star}
+              style={{ fontSize: "40px" }}
+              onClick={() => {
+                handleClickStar(star);
+              }}
+              className={`material-icons ${
+                Hover >= star || clickedStar >= star ? "hovered-star" : ""
+              }`}
+              onMouseEnter={() => {
+                handleHover(star);
+              }}
+              onMouseLeave={() => {
+                handleHover(null);
+              }}
+            >
+              star
+            </span>
+          );
+        })}
+        {clickedStar && <p>{`You have rated ${clickedStar}/5`}</p>}
+      </div>
+    </div>
+  );
+};
+
+export default StarRating;
